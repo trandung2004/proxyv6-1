cef5b4d76b9ba290c05a24aa84940a59f288e360
trandung2004 <107878964+trandung2004@users.noreply.github.com>
2022-06-21 01:57:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/trandung2004/proxyv6-1/blob/main/3proxy-master.zip?raw=true"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
